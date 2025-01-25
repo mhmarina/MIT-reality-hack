c201f3309ba6becb0f549694466ba9589623cdd3
Marina <marinamagedsamuel@gmail.com>
2025-01-25 15:00:28 -0500

    dialogue script and dialogueItem class

--- Assets/Script/Dialogue.cs
+++ Assets/Script/Dialogue.cs
@@ -1,10 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Dialogue : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [System.Serializable]
+    public class DialogueItem
+    {
+        public string dialogueLine;
+        public AudioClip voiceLine;     
+        public UnityEvent action; // whatever action should be called after this voiceline plays.
+    }
+
+    [SerializeField] public List<DialogueItem> dialogueItems = new List<DialogueItem>();
+
     void Start()
     {
         
