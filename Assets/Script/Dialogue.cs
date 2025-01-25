9dd403e9bf94eacb85a8ad1804bc4b17b50f5c22
Marina <marinamagedsamuel@gmail.com>
2025-01-25 15:45:50 -0500

    Scene1 Dialogue

--- Assets/Script/Dialogue.cs
+++ Assets/Script/Dialogue.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class Dialogue : MonoBehaviour
 {
@@ -16,15 +18,47 @@ public class Dialogue : MonoBehaviour
     }
 
     [SerializeField] public List<DialogueItem> dialogueItems = new List<DialogueItem>();
+    [SerializeField] TextMeshProUGUI captionText;
+    [SerializeField] AudioSource audioSource;
+    private int index = 0;
 
     void Start()
     {
-        
+        if (dialogueItems.Count > 0)
+        {
+            StartCoroutine(PlayDialogue());
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PlayDialogue()
     {
-        
+        while (index < dialogueItems.Count)
+        {
+            DialogueItem currentItem = dialogueItems[index];
+            captionText.text = currentItem.dialogueLine;
+
+            if (currentItem.voiceLine != null)
+            {
+                audioSource.clip = currentItem.voiceLine;
+                audioSource.Play();
+                // wait for the length of the voice clip before incrementing index
+                yield return new WaitForSeconds(audioSource.clip.length);
+            }
+            else
+            {
+                yield return new WaitForSeconds(3f); // wait arbitrary num of secs if not voice line
+            }
+
+            currentItem.action?.Invoke();
+
+            // increment index
+            index++;
+        }
+        captionText.text = ""; // Clear the caption when dialogue is finished
+    }
+
+    public void PlayNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
