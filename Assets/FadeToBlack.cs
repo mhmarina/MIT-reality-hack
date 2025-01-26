1ea4f2e8d767af41f765ff6097e67329cb4ee384
Marina <marinamagedsamuel@gmail.com>
2025-01-25 21:45:24 -0500

    Changed up fail scene

--- Assets/FadeToBlack.cs
+++ Assets/FadeToBlack.cs
@@ -7,14 +7,20 @@ using UnityEngine.UI;
 public class FadeToBlack : MonoBehaviour
 {
     [SerializeField] Image screen;
+    bool isFading = false;
 
     private void Update()
     {
-        if(screen.color.a < 1)
+        if(isFading && screen.color.a < 1)
         {
             Color color = screen.color;
             color.a = color.a + 0.1f * Time.deltaTime;
             screen.color = color;
         }
+    }
+
+    public void startFading()
+    {
+        isFading = true;
     }
 }
