ffc2018c5684ec8d345bb0bf4d577e7611d618a0
Rachel Qingzhu Zhang <rachel-qingzhuzhang@berkeley.edu>
2025-01-26 08:07:27 -0800

    changes

--- Assets/Script/SceneChanger.cs
+++ Assets/Script/SceneChanger.cs
@@ -6,12 +6,21 @@ public class SceneChangerXR : MonoBehaviour
 {
     public string targetSceneName = "YourSceneName"; // Replace with your scene name
 
+  // public GameObject xrCamera;
+
+
     // Called when the object is selected
     public void ChangeScene()
     {
         if (!string.IsNullOrEmpty(targetSceneName))
         {
-            SceneManager.LoadScene(targetSceneName);
+           // SceneManager.LoadScene(targetSceneName);
+            SceneManager.LoadSceneAsync(targetSceneName);
+
+            //xrCamera.transform.position = Vector3.zero;
+
+
+
         }
     }
 }
